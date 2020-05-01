a647eba2fcf700db1f8ae6093cd31a5496ff2c89
kumackey <kumak1t09e0@gmail.com>
2020-05-01 17:15:19 +0900

    learn ORM

--- data/htdocs/sample/src/Controller/PostsController.php
+++ data/htdocs/sample/src/Controller/PostsController.php
@@ -6,7 +6,10 @@ class PostsController extends AppController
 {
   public function index()
   {
-    $posts = $this->Posts->find('all');
+    $posts = $this->Posts->find('all')
+              ->order(['title' => 'DESC'])
+              ->limit(2)
+              ->where(['title like' => '%3']);
     $this->set(compact('posts'));
   }
 }
